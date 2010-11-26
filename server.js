37f1b79dd89a37b42dc3183240d29f40888ddfdf
rogeriopvl <rogeriopvl@gmail.com>
2010-11-26 16:12:00 +0000

    Added a index page instead of printing the content directly.

--- server.js
+++ server.js
@@ -13,9 +13,12 @@ server = http.createServer(function(req, res){
 	var path = url.parse(req.url).pathname;
 
 	if (path == '/'){
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write('<h1>Get ready to be Chatstormized! <a href="/chat">Connect!</a></h1>');
-		res.end();
+		fs.readFile(__dirname +'/public/index.html', function(err, data){
+			if (err) return send404(res);
+			res.writeHead(200, {'Content-Type': 'text/html'})
+			res.write(data, 'utf8');
+			res.end();
+		});
 	}
 	else if (path == '/chat'){
 		fs.readFile(__dirname +'/public'+path+'.html', function(err, data){
